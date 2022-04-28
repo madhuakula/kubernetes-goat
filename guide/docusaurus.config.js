43d49b3489a8570178891d478664583fbd06cd55
Madhu Akula <madhuakula@users.noreply.github.com>
2022-04-28 20:31:46 +0200

    Fixed baseUrl path for docusaurus

--- guide/docusaurus.config.js
+++ guide/docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const BASE_URL = '/';
+const BASE_URL = '/kubernetes-goat/';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
