b12711143d174e5bf2e8cf92a0cbac177126e940
Jonathan Magee <120496483+jmagee70@users.noreply.github.com>
2023-12-18 10:33:41 +0000

    Delete platforms directory

--- platforms/kind-setup/teardown-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Author: Mauricio Cano
-# This file has been created to contribute to the development of the Kubernetes Goat
-
-# Destroy cluster and delete everyting
-bash ../../teardown-kubernetes-goat.sh
-kind delete cluster --name kubernetes-goat-cluster
