b45a05aa09cb015174f00d268f6dcd714ec5c8d4
samriggs <samriggs@users.noreply.github.com>
2025-02-01 02:55:39 -0800

    Add Powershell scripts to run Kubernetes Goat in Windows

---
+++ platforms/kind-setup/teardown-cluster.ps1
@@ -0,0 +1,5 @@
+# This file has been created to contribute to the development of the Kubernetes Goat
+
+# Destroy cluster and delete everyting
+../../teardown-kubernetes-goat.ps1
+kind delete cluster --name kubernetes-goat-cluster
