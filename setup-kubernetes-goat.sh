319180635ce4bdc8c94f557fd5f59590300df1f7
bykvaadm <info@bykvaadm.ru>
2025-11-18 16:06:05 +0300

    Add insecure flag and path to kubeconfig in setup script

--- setup-kubernetes-goat.sh
+++ setup-kubernetes-goat.sh
@@ -3,8 +3,39 @@
 # This program has been created as part of Kubernetes Goat
 # Kubernetes Goat setup and manage vulnerable infrastructure
 
+INSECURE=""
+KUBECONFIG_ARG=""
+export KUBECTL_INSECURE=""
+export HELM_INSECURE=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --insecure)
+      INSECURE="yes"
+      shift
+      ;;
+    --kubeconfig)
+      KUBECONFIG_ARG="$2"
+      shift 2
+      ;;
+    *)
+      echo "unknown arg: $1"
+      shift
+      ;;
+  esac
+done
+
+if [[ -n "$KUBECONFIG_ARG" ]]; then
+  export KUBECONFIG="$KUBECONFIG_ARG"
+fi
+
+if [[ -n "$INSECURE" ]]; then
+  export KUBECTL_INSECURE="--insecure-skip-tls-verify"
+  export HELM_INSECURE="--kube-insecure-skip-tls-verify"
+fi
+
 # Checking kubectl setup
-kubectl version > /dev/null 2>&1 
+kubectl $KUBECTL_INSECURE version > /dev/null 2>&1
 if [ $? -eq 0 ];
 then
     echo "kubectl setup looks good."
@@ -35,24 +66,24 @@ fi
 
 # deploying insecure-rbac scenario
 echo "deploying insecure super admin scenario"
-kubectl apply -f scenarios/insecure-rbac/setup.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/insecure-rbac/setup.yaml
 
 # deploying helm chart to verify the setup
 echo "deploying helm chart metadata-db scenario"
-helm install metadata-db scenarios/metadata-db/
+helm $HELM_INSECURE install metadata-db scenarios/metadata-db/
 
 # setup the scenarios/configurations
 echo 'deploying the vulnerable scenarios manifests'
-kubectl apply -f scenarios/batch-check/job.yaml
-kubectl apply -f scenarios/build-code/deployment.yaml
-kubectl apply -f scenarios/cache-store/deployment.yaml
-kubectl apply -f scenarios/health-check/deployment.yaml
-kubectl apply -f scenarios/hunger-check/deployment.yaml
-kubectl apply -f scenarios/internal-proxy/deployment.yaml
-kubectl apply -f scenarios/kubernetes-goat-home/deployment.yaml
-kubectl apply -f scenarios/poor-registry/deployment.yaml
-kubectl apply -f scenarios/system-monitor/deployment.yaml
-kubectl apply -f scenarios/hidden-in-layers/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/batch-check/job.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/build-code/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/cache-store/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/health-check/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/hunger-check/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/internal-proxy/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/kubernetes-goat-home/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/poor-registry/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/system-monitor/deployment.yaml
+kubectl $KUBECTL_INSECURE apply -f scenarios/hidden-in-layers/deployment.yaml
 
 echo 'Successfully deployed Kubernetes Goat. Have fun learning Kubernetes Security!'
 echo 'Ensure pods are in running status before running access-kubernetes-goat.sh script'
